fe87061ad95a0420c8091ab7c6e74472b041a61f
Annamalia <anamalia@tut.by>
2019-03-19 23:29:07 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0,
+  check = new Boolean;
+while (i <= bracketsConfig.length - 1) {
+  bracketsConfig[i] = String(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  i++;
+}
+i = 0;
+while (i <= str.length) {
+  if (bracketsConfig.indexOf(str[i] + str[i + 1]) == -1) {
+    i++;
+  } else {
+    str = str.slice(0, i) + str.slice(i + 2, str.length);
+    i--;
+  }
+}
+if (str==="") {
+  check = true;
+} else {
+  check = false;
+}
+return check;
 }
